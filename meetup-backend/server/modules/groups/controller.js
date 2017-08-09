f015bff900164773a87c573611fc82b6dbc4b1a1
callmepaulb <paul.w.bie@gmail.com>
2017-08-09 18:55:50 +0200

    [Add] - New API for adding a new group to DB

---
+++ meetup-backend/server/modules/groups/controller.js
@@ -0,0 +1,33 @@
+import Group from './model'
+
+export const createGroup = async (req, res) => {
+   const {
+      name,
+      description,
+      category,
+   } = req.body
+
+   if (!name) {
+      return res.status(400).json({ error: true, message: 'Name must be provided!' })
+   } else if (typeof name !== 'string') {
+      return res.status(400).json({ error: true, message: 'Name must be a string!' })
+   } else if (name.length < 5) {
+      return res.status(400).json({ error: true, message: 'Name must be at least 5 characters long!' })
+   }
+
+   if (!description) {
+      return res.status(400).json({ error: true, message: 'Description must be provided!' })
+   } else if (typeof description !== 'string') {
+      return res.status(400).json({ error: true, message: 'Description must be a string!' })
+   } else if (description.length < 10) {
+      return res.status(400).json({ error: true, message: 'Description must be at least 10 characters long!' })
+   }
+
+   const group = new Group({ name, description })
+
+   try {
+      return res.status(201).json({ error: false, group: await group.save() })
+   } catch (e) {
+      return res.status(400).json({ error: true, message: 'Error when creating group' })
+   }
+}
